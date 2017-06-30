a28cae9ba931a4b1d724658f65450873ce6b11f4
zslayer06 <zfrye06@gmail.com>
2017-06-29 18:40:01 -0600

    Initial project files

---
+++ Source/ADsZNarcolepsy/ADsZNarcolepsyGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#include "ADsZNarcolepsyGameMode.h"
+#include "ADsZNarcolepsyCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AADsZNarcolepsyGameMode::AADsZNarcolepsyGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
